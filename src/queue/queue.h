278d235ca6ff17fb4830e53ac559627b3e595ad7
hellabastard <alexejbudnikow1810@gmail.com>
2025-09-20 13:53:41 +0300

    implement basic q operations

--- src/queue/queue.h
+++ src/queue/queue.h
@@ -4,5 +4,65 @@
 
 #ifndef QUEUE_H
 #define QUEUE_H
+#include <stdexcept>
+
+template<typename E>
+class Queue {
+    private:
+    const size_t capacity;
+    size_t size;
+    E* heap;
+
+    public:
+    // Constructor
+    explicit Queue(size_t cap)
+        : capacity(cap),
+        size(0),
+        heap(new E[capacity]) {}
+    // Destructor
+    ~Queue() {
+        delete[] heap;
+    }
+    // Prohibit assignment and movement
+    Queue& operator=(const Queue&) = delete;
+    Queue(const Queue&) = delete;
+    Queue& operator=(Queue&&) = delete;
+    Queue(Queue&&) = delete;
+
+    // == Basic operations ==
+    [[nodiscard]] bool is_empty() const { return size == 0; }
+
+    [[nodiscard]] bool is_full() const { return size == capacity; }
+
+    [[nodiscard]] size_t get_size() const { return size; }
+
+    [[nodiscard]] size_t get_capacity() const { return capacity; }
+
+    [[maybe_unused]] const E& peek_top() const {
+        if (is_empty()) throw std::out_of_range("Queue is empty");
+        return heap[size-1];
+    }
+
+    void push(E&& e) {
+        if (is_full()) throw std::out_of_range("Queue is full");
+        heap[size++] = std::move(e);
+        ++size;
+    }
+
+    E pop() {
+        if (is_empty()) throw std::out_of_range("Queue is empty");
+
+        E res = std::move(heap[--size]);
+
+        if (size > 1) {
+            for (size_t i = size; i > 1; --i) {
+                heap[i] = std::move(heap[i/2]);
+            }
+            --size;
+        } else size = 0;
+
+        return res;
+    }
+};
 
 #endif //QUEUE_H
