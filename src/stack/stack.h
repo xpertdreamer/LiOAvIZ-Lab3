aa9b11c0e99006f12212fa91b1eadd7be927345d
hellabastard <alexejbudnikow1810@gmail.com>
2025-09-21 18:26:24 +0300

    dynamic stack

--- src/stack/stack.h
+++ src/stack/stack.h
@@ -9,10 +9,20 @@
 template<typename E>
 class Stack {
     private:
-    const size_t capacity;  // Max size of our stack
+    size_t capacity;  // Max size of our stack
     size_t size;
     size_t top;     // Top of the stack
     E* heap;
+
+    void resize(size_t newCapacity) {
+        E* newHeap = new E[newCapacity];
+        for (size_t i = 0; i < top; i++) {
+            newHeap[i] = std::move(heap[i]);
+        }
+        delete[] heap;
+        heap = newHeap;
+        capacity = newCapacity;
+    }
 
     public:
     // Constructor
@@ -38,28 +48,30 @@ class Stack {
     [[nodiscard]] size_t get_capacity() const { return capacity; }
     // Copy existing object
     void push(const E& e) {
-        if (is_full()) throw std::out_of_range("Stack is full");
+        if (is_full()) resize(2 * capacity);
         heap[++top] = e;
         ++size;
     }
     // Move object or move temporary
     void push(E&& e) {
-        if (is_full()) throw std::out_of_range("Stack is full");
+        if (is_full()) resize(2 * capacity);
         heap[++top] = std::move(e);
         ++size;
     }
 
     [[maybe_unused]] E pop() {
         if (is_empty()) throw std::out_of_range("Stack is empty");
+
+        if (size < capacity / 4 && capacity > 10) resize(capacity / 2);
+
         --size;
-        return heap[top--];
+        return std::move(heap[top--]);
     }
 
     [[maybe_unused]] E peek() const {
         if (is_empty()) throw std::out_of_range("Stack is empty");
         return heap[top];
     }
-
 };
 
 #endif //STACK_H
